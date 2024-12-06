56729725cc7f0bd2b906f84ce673c712e73acfbb
TomyStarkCodePro <20233tn147@utez.edu.mx>
2024-12-06 00:42:21 -0600

    creacion de clase clienteController

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/controller/ClienteController.java
@@ -0,0 +1,24 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.controller;
+
+import org.example.ricardomedinatomasramirez4fintegradorai.dao.IClienteRepository;
+import org.example.ricardomedinatomasramirez4fintegradorai.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    @Autowired
+    private IClienteRepository clienteRepository;
+
+    @PostMapping("/agregarCliente")
+    public ResponseEntity<Cliente> agregarCliente(@RequestBody Cliente cliente) {
+        Cliente nuevoCliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(nuevoCliente);
+    }
+}
