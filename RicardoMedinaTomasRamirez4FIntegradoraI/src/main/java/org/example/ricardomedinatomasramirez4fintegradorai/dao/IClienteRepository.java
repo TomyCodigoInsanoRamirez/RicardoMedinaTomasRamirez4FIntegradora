d51abb87de9481aa374673f82a4ae9c130001398
TomyStarkCodePro <20233tn147@utez.edu.mx>
2024-12-06 00:40:09 -0600

    creacion de interdaz IClienteRepository

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/dao/IClienteRepository.java
@@ -0,0 +1,9 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.dao;
+
+import org.example.ricardomedinatomasramirez4fintegradorai.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
+
