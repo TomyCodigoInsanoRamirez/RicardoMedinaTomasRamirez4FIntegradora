47fd5bb2766845d44e2c5c5ac76322d87c84339f
TomyStarkCodePro <20233tn147@utez.edu.mx>
2024-12-06 00:33:23 -0600

    creacion de modelo carrito producto

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/model/CarritoProducto.java
@@ -0,0 +1,55 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "carrito_producto")
+public class CarritoProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    @Column(nullable = false)
+    private int cantidad;
+
+    // Getters y Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+}
