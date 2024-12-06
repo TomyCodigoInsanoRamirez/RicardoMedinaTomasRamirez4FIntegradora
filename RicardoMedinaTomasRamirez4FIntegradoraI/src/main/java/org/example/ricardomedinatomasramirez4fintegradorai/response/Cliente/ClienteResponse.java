63a239a354e21b26a0f25467466de49321c6a2d3
TomyStarkCodePro <20233tn147@utez.edu.mx>
2024-12-06 00:37:32 -0600

    creacion de clase cliente response

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/response/Cliente/ClienteResponse.java
@@ -0,0 +1,15 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.response.Cliente;
+
+import org.example.ricardomedinatomasramirez4fintegradorai.model.Cliente;
+
+import java.util.List;
+
+public class ClienteResponse {
+    private List<Cliente> clientes;
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+}
