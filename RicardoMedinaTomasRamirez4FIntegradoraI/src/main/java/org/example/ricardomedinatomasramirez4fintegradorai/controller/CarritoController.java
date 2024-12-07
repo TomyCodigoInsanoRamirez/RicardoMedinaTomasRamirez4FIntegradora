536be641cb37fe9241b0ce195eaf54cc8cc70c09
TomyStarkCodePro <20233tn147@utez.edu.mx>
2024-12-07 00:59:16 -0600

    Se agrego carrito controller

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/controller/CarritoController.java
@@ -0,0 +1,23 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.controller;
+
+import org.example.ricardomedinatomasramirez4fintegradorai.model.CarritoProducto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/carrito")
+public class CarritoController {
+    @Autowired
+    private ICarritoRepository carritoRepository;
+
+    @PostMapping("/agregar")
+    public ResponseEntity<CarritoProducto> agregarItemCarro(@RequestBody CarritoProducto itemAdd) {
+        CarritoProducto carritoProducto = carritoRepository.save(itemAdd);
+        return ResponseEntity.ok(carritoProducto);
+    }
+}
