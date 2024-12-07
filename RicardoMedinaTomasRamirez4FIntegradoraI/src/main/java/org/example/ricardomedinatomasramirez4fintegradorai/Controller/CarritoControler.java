093278a7e357c8949fbc748d5f7058ecda983023
RichartM <https://github.com/RichartM/PruebaN.git>
2024-12-07 06:32:37 -0600

    CarroController

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/Controller/CarritoControler.java
@@ -0,0 +1,19 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/carrito")
+public class CarritoControler {
+    @Autowired
+    private ICarritoRepository carritoRepository;
+
+    @PostMapping("/agregar")
+    public ResponseEntity<CarritoProducto> agregarItemCarro(@RequestBody CarritoProducto itemAdd) {
+        CarritoProducto carritoProducto = carritoRepository.save(itemAdd);
+        return ResponseEntity.ok(carritoProducto);
+    }
+}
+
