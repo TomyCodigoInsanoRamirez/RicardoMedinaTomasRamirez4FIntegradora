209824fc795e607ee1efb57658d4e82856cc340c
RichartM <https://github.com/RichartM/PruebaN.git>
2024-12-07 06:36:28 -0600

    ModelCliente

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/model/Cliente.java
@@ -0,0 +1,97 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String primerNombre;
+
+    private String segundoNombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+
+    @Column(nullable = false)
+    private String estadoNacimiento;
+
+    @Temporal(TemporalType.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date fechaNacimiento;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CarritoProducto> carritoProductos;
+
+    // Getters y Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEstadoNacimiento() {
+        return estadoNacimiento;
+    }
+
+    public void setEstadoNacimiento(String estadoNacimiento) {
+        this.estadoNacimiento = estadoNacimiento;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public List<CarritoProducto> getCarritoProductos() {
+        return carritoProductos;
+    }
+
+    public void setCarritoProductos(List<CarritoProducto> carritoProductos) {
+        this.carritoProductos = carritoProductos;
+    }
+}
