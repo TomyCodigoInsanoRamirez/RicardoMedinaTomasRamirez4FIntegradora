805d0f8f8b7f45cbb924bca2077d3e120a6b694e
RichartM <https://github.com/RichartM/PruebaN.git>
2024-12-07 06:38:04 -0600

    DaoCliente

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/dao/IClienteRepository.java
@@ -0,0 +1,10 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.dao;
+
+import org.example.ricardomedinatomasramirez4fintegradorai.dao.IClienteRepository;
+import org.example.ricardomedinatomasramirez4fintegradorai.model.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface IClienteRepository extends CrudRepository<Cliente, Long> {
+
+}
