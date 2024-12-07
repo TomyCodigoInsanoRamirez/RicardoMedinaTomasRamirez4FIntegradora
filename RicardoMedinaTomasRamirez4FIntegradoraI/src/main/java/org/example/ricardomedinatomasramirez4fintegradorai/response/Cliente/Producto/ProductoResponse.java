64afadd519873537a6cdd0a9f34816ebb116a884
RichartM <https://github.com/RichartM/PruebaN.git>
2024-12-07 08:19:48 -0600

    ProductoResponse

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/response/Cliente/Producto/ProductoResponse.java
@@ -0,0 +1,14 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.response.Cliente.Producto;
+
+
+import java.util.List;
+
+public class ProductoResponse {
+    private List<Producto> productos;
+    public List<Producto> getProductos() {
+        return productos;
+    }
+    public void setProductos(List<Producto> producto) {
+        this.productos = productos;
+    }
+}
