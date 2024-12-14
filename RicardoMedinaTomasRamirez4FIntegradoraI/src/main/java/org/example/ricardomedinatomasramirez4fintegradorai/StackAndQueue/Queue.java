6eccf1a3a6f088f8e46999278938a294755a922a
TomyStarkCodePro <20233tn147@utez.edu.mx>
2024-12-13 21:40:47 -0600

    queue y stack custom

---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/StackAndQueue/Queue.java
@@ -0,0 +1,55 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.StackAndQueue;
+
+public class Queue<T> {
+    int tail; //index de cola
+    int head; //index de la cabeza
+    int itemsNumber;// numero de elementos actuales en la fila
+    int arrLength;//capacidad de la fila
+    T[] items;//items
+
+    Queue( int arrLength){
+        this.arrLength = arrLength;
+        this.head = 0;
+        this.tail = -1;
+        this.itemsNumber = 0;
+        this.items = (T[]) new Object[arrLength];
+    }
+
+    //metodo para meter un elemento a la queue
+    public void offer(T item){
+        if(isFull()){
+            System.out.println("La queue esta llena");
+            return;
+        }
+        tail=(tail+1)%arrLength;//indicador de tail en forma circular(aumenta la cola)
+        items[tail] = item;//setea el nuevo item en la queue
+        itemsNumber++;//actualiza el numero de items en la queue
+    }
+
+    public T poll(){//poll recupera y elimina
+        if(isEmpty()){
+            System.out.println("La queue esta vacia");
+            return null;
+        }
+        T item = items[head];//seleccionar el item en la cabeza de la queue
+        head = (head+1)%arrLength;//actualizar el indicador de forma circular
+        itemsNumber--;//actualizar el numero de items en la queue
+        return item;//retorna el primer elemento en la queue
+    }
+
+    public T peek(){// ver el elemento al frente de la cola sin eliminarlo.
+        if(isEmpty()){
+            System.out.println("La queue esta vacia");
+            return null;
+        }
+        return items[head];
+    }
+
+    public boolean isEmpty(){//verificar que la cola esta vacia
+        return (itemsNumber == 0);
+    }
+
+    public boolean isFull(){//verificar si la queue esta llena
+        return (itemsNumber == arrLength);
+    }
+}
---
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/StackAndQueue/Stack.java
@@ -0,0 +1,4 @@
+package org.example.ricardomedinatomasramirez4fintegradorai.StackAndQueue;
+
+public class Stack {
+}
