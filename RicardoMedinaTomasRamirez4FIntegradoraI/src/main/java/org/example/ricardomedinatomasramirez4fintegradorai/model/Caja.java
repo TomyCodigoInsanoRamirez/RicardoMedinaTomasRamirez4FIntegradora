452347d8a7e4085a4300064382611ca340e5aae5
RichartM <20233tn230@utez.edu.mx>
2024-12-14 08:42:40 -0600

    ihrfb

--- RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/model/Caja.java
+++ RicardoMedinaTomasRamirez4FIntegradoraI/src/main/java/org/example/ricardomedinatomasramirez4fintegradorai/model/Caja.java
@@ -51,6 +51,6 @@ public class Caja {
         return "Caja{" +
                 "id=" + id +
                 ", nombre='" + nombre + '\'' +
-                '}';
-    }
+                    '}';
+        }
 }
